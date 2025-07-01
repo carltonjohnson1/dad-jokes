7c305021bad489e1ae801a9b419ba9e074b0999a
carltonjohnson1 <carltonj1@hotmail.com>
2025-07-01 07:55:26 +0100

    added audio update and various other updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,28 @@
-
-
 document.getElementById("btn").addEventListener("click", joke);
 
+// Sound toggle logic
+const soundDiv = document.getElementById("sound");
+const soundImg = soundDiv.querySelector("img");
+let isSoundOn = false; // When false, audio is muted; when true, audio is unmuted
+
+soundDiv.addEventListener("click", () => {
+    isSoundOn = !isSoundOn;
+    if (isSoundOn) {
+        soundImg.src = "images/sound-off.svg";
+        soundImg.alt = "Sound Off";
+    } else {
+        soundImg.src = "images/sound-on.svg";
+        soundImg.alt = "Sound On";
+    }
+});
+
 async function joke() {
+    // Play the child-laughing audio only if sound is ON (sound-off image is visible)
+    const audio = document.querySelector(".play-sound");
+    if (audio && isSoundOn) {
+        audio.currentTime = 0; // rewind to start
+        audio.play();
+    }
     let config = {
         headers: {
             accept: "application/json",
@@ -13,3 +33,5 @@ async function joke() {
     let b = await a.json();
     document.getElementById("content"). innerHTML = b.joke;
 }
+
+
